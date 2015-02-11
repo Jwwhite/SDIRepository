0e0b12e788e3eaf953767bee684dbcd9d57ab697
Jwwhite <jwwhite@fullsail.edu>
2015-02-11 01:16:30 -0600

    Used the Console and Alerts
    Added all examples from the video to the javaScript file.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -7,3 +7,12 @@ var yearBorn = 1955;
 
 // Displaying a string of text to the console
 console.log("Hello World");
+
+// Displaying the variable yearBorn to the console
+console.log(yearBorn);
+
+// Using an alert to display text
+alert("Hello World");
+
+// Using an alert to display the variable yearBorn
+alert(yearBorn);
