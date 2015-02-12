b4f1967af1965982d49786531b7b931a17a2eee8
Jwwhite <jwwhite@fullsail.edu>
2015-02-11 18:53:34 -0600

    Concatenating
    Followed the screen cast on concatenation.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -4,11 +4,22 @@
 
 // alert("JS is working");
 
-// Creates an array that contains the how many oranges are in each of the three bins
-var orangeBins = [234, 567, 883];
+// Declares variables to store strings for concatenation
+var firstName = "Jesse";
+var lastName = "White";
 
-// Creates a variable that contains the sum of all three bins
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+// Concatenates variables firstName and lastName with a space between
+var fullName = firstName + " " + lastName;
 
-// Displays the variable total to the console
-console.log(total);
+// Displays the result of the variable fullName to the console
+console.log(fullName);
+
+// Declares variables to express how concatenating is not addition
+var a = "6";
+var b = "7";
+
+//  Concatenates variables a and b together
+var result = a + b;
+
+// Displays variable result to the console
+console.log(result);
