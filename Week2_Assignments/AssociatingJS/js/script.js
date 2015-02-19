84be85802a878938d61ba22a64c47adaab4b3169
Jwwhite <jwwhite@fullsail.edu>
2015-02-18 21:47:22 -0600

    Else if
    Followed the screencast on else if.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -6,9 +6,12 @@
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHieght = 45;
 
 if (kidHeight > minHeight) {
     console.log("You can ride the coaster!")
+} else if (kidHeight > wParentHieght) {
+    console.log("You can ride, but only with a parent present.")
 } else {
     console.log("Sorry kid, you've got some growing to do first!");
 }
