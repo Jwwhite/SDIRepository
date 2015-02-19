a4439738c90593128caa2673e49109a270d753df
Jwwhite <jwwhite@fullsail.edu>
2015-02-18 22:29:41 -0600

    Ternary Operators
    Followed the screencast on ternary operators.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -4,12 +4,11 @@
 
 // alert("JS is working");
 
-var budget = 100;
-var iPhonePrice = 199.99;
-var wonLottery = true;
+var gpa = 48;
 
-if (iPhonePrice < budget || wonLottery) {
-    console.log("We can buy the phone!")
-} else {
-    console.log("No phone for you!!")
-}
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+
+age = 6;
+var book;
+
+book = (age < 10) ? "Green eggs and ham" : "The time machine";
