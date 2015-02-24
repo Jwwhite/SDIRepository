6cfb52ce40801344ac574983615a320ae4fb3af5
Jwwhite <jwwhite@fullsail.edu>
2015-02-24 16:17:58 -0600

    Variable Scope
    Followed the screencast variable scope.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -4,18 +4,16 @@
 
 // alert("JS is working");
 
-function outptMsg() {
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea() {
     var width = 20;
     var height = 30;
     var area = width * height;
 
-    console.log(area);
+    //console.log(area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+
+console.log(width);
