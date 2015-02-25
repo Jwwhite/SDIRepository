abf599ea8deb9ae44cd1a405b792346eb6fce435
Jwwhite <jwwhite@fullsail.edu>
2015-02-24 21:37:48 -0600

    Do While Loops
    Followed the screencast on do while loops.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -4,9 +4,16 @@
 
 // alert("JS is working");
 
-var b = 50;
+var b = 10;
 
 while (b > 0) {
     console.log(b + " Kegs on the wall.");
     b--;
 }
+
+var c = 10;
+
+do {
+    console.log(c + " Kegs on the wall.");
+
+} while (c > 10);
