edb9911f454240c7f642134d3c0bb151946fa4c9
Jwwhite <jwwhite@fullsail.edu>
2015-02-24 21:53:35 -0600

    For Loop
    Followed the screencast on for loops and fixed the do while loop not
    counting down.

--- Week2_Assignments/AssociatingJS/js/script.js
+++ Week2_Assignments/AssociatingJS/js/script.js
@@ -4,16 +4,24 @@
 
 // alert("JS is working");
 
+// while loop
 var b = 10;
 
 while (b > 0) {
-    console.log(b + " Kegs on the wall.");
+    console.log(b + " Kegs on the wall. w");
     b--;
 }
 
+// Do while loop
 var c = 10;
 
 do {
-    console.log(c + " Kegs on the wall.");
+    console.log(c + " Kegs on the wall. dw");
+    c--;
+}
+while (c > 0);
 
-} while (c > 10);
+// for loop
+for (i = 10; i > 0; i--) {
+    console.log(i + " Kegs on the wall. f");
+}
