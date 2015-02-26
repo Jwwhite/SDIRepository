946e986d538cfe5f8b7dbde7a60b3ef501a352e2
Jwwhite <jwwhite@fullsail.edu>
2015-02-26 03:51:49 -0600

    Week 4 Assignment: Functions
    I realized I was reusing a lot of code so I transferred it into a
    function to make the program more efficient. I also debugged, removed
    the console logs and finished commenting.

--- Week4_Assignments/White_Jesse_Functions_Assignment/js/lottery.js
+++ Week4_Assignments/White_Jesse_Functions_Assignment/js/lottery.js
@@ -5,89 +5,88 @@
 
 // Sets up the variables
 var validation;
-var select;
-var noStore;
-var i = 0;
-var floridaNo = 1;
-var powerballNo = 1;
-var noList = [];
-var lottoNo = [];
+var lottoNumbers;
+var florida = 1;
+var floridaNo = 53;
+var powerball = 1;
+var powerballNo = 59;
 var userInput = prompt("What lottery numbers would you like? " +
     "The Florida lottery or the Powerball?\n\n\t\t\t(Type \"F\"" +
     " for Florida or \"P\" for the Powerball.)");
 
 // a function for validating user input
 function validate(userIn) {
+
     // Validates the user input
     while (userIn == "") {
         userIn = prompt("What lottery numbers would you like? " +
         "The Florida lottery or the Powerball?\n\n\t\t\t(Type \"F\"" +
         " for Florida or \"P\" for the Powerball.)");
     }
-
+    // if the user input equals F or f
     if (userIn == "F" || userIn == "f") {
-        return "F";
+        return "F"; // return an uppercase F
 
+    // else if the user input equals P or p
     } else if (userIn == "P" || userIn == "p") {
-        return "P";
+        return "P"; // return an uppercase P
     }
 
+}
+
+function numberGen(lottoType, numbersToGen) {
+    var select;
+    var noStore;
+    var i = 0;
+    var noList = [];
+    var lottoNo = [];
+
+    // while the lottery type is less than or equal to how many numbers to generate
+    while (lottoType <= numbersToGen) {
+        noList[i] = lottoType; // adds the new number to the array
+        i++;                   // adds one to the i variable
+        lottoType++;           // adds one to the lottery numbers
+
+    }
+
+    // var i equals 0, while i is less than or equals 5, add one to i
+    for (i = 0; i <= 5; i++) {
+        select = Math.floor(Math.random() * (lottoType - 1)); // select a number randomly from one to the max for the lottery
+        noStore = noList[select]; // uses the random number from above and selects a number from the array
+        noList.splice(select, 1); // removes the number selected
+        while (noStore === undefined) {                            // if the number selected equals undefined
+            select = Math.floor(Math.random() * (lottoType - 1));  // repeat the selection process
+            noStore = noList[select];
+
+        }
+
+        lottoNo[i] = noStore; // store the filtered number in the indexed location
+    }
+    // return the generated lottery numbers
+    return lottoNo;
 }
 
 // uses the validate function to check the user
 // input and store it in the variable validation
 validation = validate(userInput);
 
+// if validation equals F
 if (validation == "F") {
-    // while the floridaNo is less than or equal to 52
-    while (floridaNo <= 53) {
-        noList[i] = floridaNo; // adds the new number to the array
-        i++;                   // adds one to the i variable
-        floridaNo++;           // adds one to the floridaNo variable
-    }
-    console.log(noList);
-    // var i equals 0, while i is less than or equals 5, add one to i
-    for (i = 0; i <= 5; i++) {
-        select = Math.floor(Math.random() * (floridaNo - 1)); // select a number randomly from one to 53 - 1 for every loop
-        noStore = noList[select]; // uses the random number from above and selects a number from the array
-        noList.splice(select, 1); // removes the number selected
-        while (noStore === undefined) {                            // if the number selected equals undefined
-            select = Math.floor(Math.random() * (floridaNo - 1));
-            noStore = noList[select];
-            console.log(noStore + " un");
-        }
+    // call number generator to generate an array for florida
+    lottoNumbers = numberGen(florida, floridaNo);
 
-        lottoNo[i] = noStore; // store the filtered number in the indexed location
-        console.log(lottoNo);
-    }
-
+// else if validation equals P
 } else if (validation == "P") {
-    // var i equals 0, while i is less than or equals 59, add one to i
-    while (powerballNo <= 59) {
-        noList[i] = powerballNo; // adds the new number to the array
-        i++;                     // adds one to the i variable
-        powerballNo++;           // adds one to the powerballNo
-    }
-    console.log(noList);
-    // var i equals 0, while i is less than or equals 5, add one to i
-    for (i = 0; i <= 5; i++) {
-        select = Math.floor(Math.random() * (powerballNo - 1)); // select a number randomly from one to 59 - 1 for every loop
-        noStore = noList[select]; // uses the random number from above and selects a number from the array
-        noList.splice(select, 1); // removes the number selected
-        while (noStore === undefined) {                            // if the number selected equals undefined
-            select = Math.floor(Math.random() * (powerballNo - 1));
-            noStore = noList[select];
-            console.log(noStore + " un");
-        }
+    // call number generator to generate an array for the powerball
+    lottoNumbers = numberGen(powerball, powerballNo);
 
-        lottoNo[i] = noStore; // store the filtered number in the indexed location
-        console.log(lottoNo);
+    // while the last number is greater then 35 re-randomize
+    while (lottoNumbers[5] > 35) {
+        lottoNumbers = numberGen(powerball, powerballNo);
     }
 
 }
-
-
-
-console.log("Your lottery Numbers are " + lottoNo[0] + "-" + lottoNo[1]
-    + "-" + lottoNo[2] + "-" + lottoNo[3] + "-" + lottoNo[4] + "-"
-    + lottoNo[5] + ".");
+// print the result to the user
+console.log("Your lottery Numbers are " + lottoNumbers[0] + "-" + lottoNumbers[1]
+    + "-" + lottoNumbers[2] + "-" + lottoNumbers[3] + "-" + lottoNumbers[4] + "-"
+    + lottoNumbers[5] + ".");
